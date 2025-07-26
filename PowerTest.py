690217e6922036d2f55e549feba64502df257994
Swayambhudutta <swayambhu.dutta@pwc.com>
2025-07-27 02:24:48 +0530

    Update PowerTest.py

--- PowerTest.py
+++ PowerTest.py
@@ -1,12 +1,8 @@
-# test_app.py
-
 import streamlit as st
-import pandas as pd
 
 st.title("✅ Streamlit Test App")
 st.write("If you see this, your environment is working!")
 
 uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx"])
 if uploaded_file:
-    df = pd.read_excel(uploaded_file)
-    st.write(df.head())
+    st.success("File uploaded successfully!")
